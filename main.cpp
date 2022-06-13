478294c48b667975382347b1bc801a350e1bda1f
sukeroV <he429855@gmail.com>
2022-06-13 22:30:32 +0800

    node

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
-#include "TT220611/T1.h"
-#include "TT220611/T2.h"
 using namespace std;
 
 
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    //21美团秋招
-    //T220611::T1 *t1 = new T220611::T1();
-
-    T220611::T2 *t2 = new T220611::T2();
 
 
     return 0;
