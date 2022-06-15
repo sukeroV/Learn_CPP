fa2516683008fdac4a54e172ae1db129be21df6b
sukeroV <he429855@gmail.com>
2022-06-15 22:20:11 +0800

    node

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class a{
+public:
+    a(){
+        cout << "a" << endl;
+    }
+
+};
 
 int main(int argc, char *argv[]) {
-
+    a aa;
+    a aaa = aa;
     return 0;
 }
