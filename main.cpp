b15a53ce5d2130236b462d7815143f84d0f8739d
sukeroV <he429855@gmail.com>
2022-06-16 20:54:55 +0800

    容器

--- main.cpp
+++ main.cpp
@@ -1,59 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
+void vecV(int i){
+    cout << i << endl;
+}
 
 int main(int argc, char *argv[]) {
-    //mA nB
-    int m=3,n=3;
-    //scanf("m=%d,n=%d",&m,&n);
-    cout << m << n << endl;
-    int A[m+n],B[n];
-    for (int i = 0; i < m+n; ++i) {
-        A[i] = 0;
-    }
-    for (int i = 0; i < n; ++i) {
-        B[i] = 0;
-    }
 
-    int Temp1=0;
-    int Temp2;
-//    cin >> Temp2;
-//    cout << Temp2 << endl;
-    switch (m) {
-        case 1:
-            scanf("%d",&A[0]);
-            break;
-        case 2:
-            scanf("%d,%d",&A[0],&A[1]);
-            break;
-        case 3:
-            scanf("%d,%d,%d",&A[0],&A[1],&A[2]);
-            break;
-        case 4:
-            scanf("%d,%d,%d,%d",&A[0],&A[1],&A[2],&A[3]);
-            break;
-    }
-    switch (n) {
-        case 1:
-            scanf("%d",&A[0]);
-            break;
-        case 2:
-            scanf("%d,%d",&A[0],&A[1]);
-            break;
-        case 3:
-            scanf("%d,%d,%d",&A[0],&A[1],&A[2]);
-            break;
-        case 4:
-            scanf("%d,%d,%d,%d",&A[0],&A[1],&A[2],&A[3]);
-            break;
-    }
-
-
-
-
-
-
-
+    vector<int> vec;
+    vec.reserve(80);
+    vec.push_back(10);
+    vec.push_back(12);
+    vec.push_back(1);
+    vec.push_back(100);
+    vector<int> vec2 = vec;
+    int Temp = 10000;
+    vec.insert(vec.cbegin(),Temp);
+    auto *au = vec.data();
+    cout << *au;
+    vec[1] = 123;
 
 
     return 0;
